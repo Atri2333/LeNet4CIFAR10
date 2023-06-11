3653c12631142a31f12bd0159b84f4720b5bc387
Atri2333 <AtriKawaii@outlook.com>
2023-06-11 22:22:16 +0800

    modify test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from torchvision import transforms
 from model import *
 
 lenet = LeNet()
+#choose one model from 100(=epoch) models
 lenet.load_state_dict(torch.load("model/Trained_Lenet81.pth"))
 lenet.eval()
 
@@ -12,19 +13,17 @@ trans = transforms.Compose([
     transforms.Resize((32, 32)),
     transforms.ToTensor()
 ])
-trans2 = transforms.Resize((32, 32))
 
 idx_to_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 file_path = "img"
-img_name = "8.jpg"
+img_name = "1.jpg"
+
 path = os.path.join(file_path, img_name)
 img = Image.open(path)
-img2 = img
-img2 = trans2(img2)
-img2.show()
 img_tensor = trans(img)
 img_tensor = torch.reshape(img_tensor, (1, 3, 32, 32))
+
 with torch.no_grad():
     output = lenet(img_tensor)
-    print(output)
+    #print(output)
     print(idx_to_class[torch.argmax(output).item()])
